c27c6af30ee5902fd7d57b268d796eff25878380
Fabrizio Cacicia <mail@fabriziocacicia.com>
2023-03-04 10:51:33 +0100

    style: format the sources with npm run format (#14)

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
 import * as core from '@actions/core'
-import { compare } from './compare';
+import {compare} from './compare'
 
 async function run(): Promise<void> {
-    try {
-        const firstToCompare: string = core.getInput('first');
-        const secondToCompare: string = core.getInput('second');
-        var operator: string = core.getInput('operator');
+  try {
+    const firstToCompare: string = core.getInput('first')
+    const secondToCompare: string = core.getInput('second')
+    var operator: string = core.getInput('operator')
 
-        const result = compare(firstToCompare, secondToCompare, operator);
-        core.setOutput("result", result);
-        console.log(`'${firstToCompare} ${operator} ${secondToCompare}' comparison result is ${result}`);
-        
-    } catch (error) {
-        if (error instanceof Error) core.setFailed(error.message);
-    }
+    const result = compare(firstToCompare, secondToCompare, operator)
+    core.setOutput('result', result)
+    console.log(
+      `'${firstToCompare} ${operator} ${secondToCompare}' comparison result is ${result}`
+    )
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
 }
 
-run();
+run()
