30a8d2ce907db44c8cb7057012e9763a6a97eeb3
Fabrizio Cacicia <mail@fabriziocacicia.com>
2023-03-04 10:50:27 +0100

    style: format the sources with npm run format

--- src/compare.ts
+++ src/compare.ts
@@ -1,27 +1,36 @@
-import * as compareVersions from "compare-versions";
+import * as compareVersions from 'compare-versions'
 
 function isValidOperator(operator: string): boolean {
-    return ['>', '<', '=', '<=', '>='].includes(operator);
+  return ['>', '<', '=', '<=', '>='].includes(operator)
 }
 
-export function compare(firstToCompare: string, secondToCompare: string, operator: string): boolean {
-    if(!firstToCompare){
-        throw new Error("'first' input is missing")
-    }
+export function compare(
+  firstToCompare: string,
+  secondToCompare: string,
+  operator: string
+): boolean {
+  if (!firstToCompare) {
+    throw new Error("'first' input is missing")
+  }
 
-    if(!secondToCompare){
-        throw new Error("'second' input is missing")
-    }
-    
-    if(operator) {
-        if(!isValidOperator(operator)) {
-            throw new Error(`Unvalid operator ${operator}. Only the following ar allowed: '>', '<', '=', '<=', '>='`)
-        }
-    } else {
-        operator = '>'
-    }
+  if (!secondToCompare) {
+    throw new Error("'second' input is missing")
+  }
 
-    // Cast to CompareOperator can be made since the `operator` content has been already checked.
-    return compareVersions.compare(firstToCompare, secondToCompare, operator as compareVersions.CompareOperator)
+  if (operator) {
+    if (!isValidOperator(operator)) {
+      throw new Error(
+        `Unvalid operator ${operator}. Only the following ar allowed: '>', '<', '=', '<=', '>='`
+      )
+    }
+  } else {
+    operator = '>'
+  }
+
+  // Cast to CompareOperator can be made since the `operator` content has been already checked.
+  return compareVersions.compare(
+    firstToCompare,
+    secondToCompare,
+    operator as compareVersions.CompareOperator
+  )
 }
-
